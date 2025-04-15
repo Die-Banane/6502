1196e1720abf1e8508bf5122d6926fd45e07edef
Jonas Grammann <jonasgrammann@gmail.com>
2025-04-15 19:35:23 +0200

    kleine optimierung

--- 6502/Processor/ALU.cs
+++ 6502/Processor/ALU.cs
@@ -8,18 +8,16 @@ namespace _6502.Processor
 {
     internal class ALU
     {
-        public void ADDC(byte value)
+        public void ADC(byte value)
         {
             byte carry = (CPU.PS.C ? (byte)1 : (byte)0);
-            byte result = (byte)(CPU.A + value + carry);
+            CPU.A = (byte)(CPU.A + value + carry);
 
             //set Flags
             CPU.PS.C = (CPU.A + value + carry) > 0xff;
-            CPU.PS.Z = result == 0;
-            CPU.PS.N = result >= 0x80;
-            CPU.PS.V = (~(CPU.A ^ value) & (CPU.A ^ result) & 0x80) != 0;
-
-            CPU.A = result;
+            CPU.PS.Z = CPU.A == 0;
+            CPU.PS.N = CPU.A >= 0x80;
+            CPU.PS.V = (~(CPU.A ^ value) & (CPU.A ^ CPU.A) & 0x80) != 0;
         }
     }
 }
